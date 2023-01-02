b39c57c359cd8379fa319dc4a928c5a9f62642cc
Ok Jae Ook <okjaeook98@gmail.com>
2023-01-02 19:53:10 +0900

    20230102

---
+++ baekjoon/P1008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class P1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble();
+        System.out.println(a / b);
+        sc.close();
+    }
+}
