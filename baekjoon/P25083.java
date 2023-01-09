a5d785a17e8773f26388f159e098dff0244b02c9
Ok Jae Ook <okjaeook98@gmail.com>
2023-01-09 20:46:00 +0900

    20230109

---
+++ baekjoon/P25083.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class P25083 {
+    public static void main(String[] args) {
+        List<String> imoji = Arrays.asList("         ,r\'\"7", "r`-_   ,'  ,/", " \\. \". L_r\'", "   `~\\/", "      |", "      |");
+        imoji.forEach(System.out::println);
+    }
+}
