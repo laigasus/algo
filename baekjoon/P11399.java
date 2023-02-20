3e181cd845b2277fc7a0a567bc63d9ca83d1d15f
Ok Jae Ook <okjaeook98@gmail.com>
2023-02-20 16:56:38 +0900

    20230220

---
+++ baekjoon/P11399.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        Arrays.sort(arr);
+        int sum = arr[0];
+        for (int i = 1; i < N; i++) {
+            arr[i] += arr[i - 1];
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
