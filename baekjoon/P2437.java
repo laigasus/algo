d3b94c3f648692220a3f979717728369ef167442
laigasus <okjaeook98@gmail.com>
2023-04-03 14:58:35 +0900

    20230402

---
+++ baekjoon/P2437.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P2437 {
+	static int N;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		int sum = 1;
+		for (int i = 0; i < arr.length; i++) {
+			if (sum < arr[i]) {
+				break;
+			}
+			sum += arr[i];
+		}
+		System.out.println(sum);
+	}
+}
