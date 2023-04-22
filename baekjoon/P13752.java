3090826cf2ba752fb824b3ab231f7905feac5f7b
laigasus <okjaeook98@gmail.com>
2023-04-22 19:40:10 +0900

    20230422

---
+++ baekjoon/P13752.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P13752 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            sb.append("=".repeat(Integer.parseInt(br.readLine()))).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
