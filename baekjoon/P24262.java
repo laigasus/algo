7f4ca2fef02d47561e5363d4a18522e704c71d25
laigasus <okjaeook98@gmail.com>
2023-05-10 14:31:32 +0900

    update P24262

--- baekjoon/P24262.java
+++ baekjoon/P24262.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class P24262 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
         System.out.println(1);
         System.out.println(0);
     }
