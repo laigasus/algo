4aa984cd1a251bda10aa21f8e2664f6630917afd
laigasus <okjaeook98@gmail.com>
2023-05-13 20:00:33 +0900

    20230513

---
+++ baekjoon/P2163.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        count = m * n - 1;
+        System.out.println(count);
+    }
+
+}
