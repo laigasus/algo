d9c3480fe31ff7cd7123c9ec3ea1e1ec3960ec41
laigasus <okjaeook98@gmail.com>
2023-05-14 19:47:48 +0900

    20230514

---
+++ baekjoon/P24723.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class P24723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(1 << sc.nextInt());
+        sc.close();
+    }
+}
