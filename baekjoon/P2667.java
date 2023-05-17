d1f925a5ac7184b86cad9ef263f574d7ef07204b
laigasus <okjaeook98@gmail.com>
2023-05-17 18:47:18 +0900

    20230517

---
+++ baekjoon/P2667.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P2667 {
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static List<Integer> homes = new ArrayList<>();
+    static int cnt = 0;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (graph[i][j] == 1 && !visited[i][j]) {
+                    dfs(i, j);
+                    homes.add(cnt);
+                    cnt = 0;
+                } else {
+                    visited[i][j] = true;
+                }
+
+            }
+        }
+
+        Collections.sort(homes);
+
+        sb.append(homes.size()).append('\n');
+        homes.forEach(n -> sb.append(n).append('\n'));
+
+        System.out.print(sb);
+    }
+
+    static void dfs(int y, int x) {
+        cnt++;
+        visited[y][x] = true;
+        int nx, ny;
+
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                continue;
+            }
+            if (graph[ny][nx] == 1 && !visited[ny][nx]) {
+                dfs(ny, nx);
+            }
+        }
+    }
+}
