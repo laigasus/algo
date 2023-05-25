2f67dbddcc8dacaf514b680f24fcbf1a8031a747
laigasus <okjaeook98@gmail.com>
2023-05-25 22:09:11 +0900

    20230525

---
+++ baekjoon/P1026.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class P1026 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        Integer[] B = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B, Comparator.reverseOrder());
+
+        int rs = 0;
+        for (int i = 0; i < N; i++) {
+            rs += A[i] * B[i];
+        }
+
+        System.out.println(rs);
+    }
+}
