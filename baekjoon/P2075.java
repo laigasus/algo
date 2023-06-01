3a53d8d55868e0baed7da25435660fef8edfcf75
laigasus <okjaeook98@gmail.com>
2023-06-01 21:11:19 +0900

    20230601

---
+++ baekjoon/P2075.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(N);
+
+        for (String s : br.readLine().split(" ")) {
+            pq.offer(Integer.parseInt(s));
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (String s : br.readLine().split(" ")) {
+                int num = Integer.parseInt(s);
+                if (pq.peek() < num) {
+                    pq.poll();
+                    pq.offer(num);
+                }
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
