fa1f1e07f72ebff50fc0329d882a4a49e5d20d6d
laigasus <okjaeook98@gmail.com>
2023-06-12 00:11:05 +0900

    20230611

--- baekjoon/P1003.java
+++ baekjoon/P1003.java
@@ -1,41 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+
 public class P1003 {
- 
-	static int zero;
-	static int one;
-	static int zero_plus_one;
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int T = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < T; i++) {
-			int N = Integer.parseInt(br.readLine());
-			fibonacci(N);			
-			sb.append(zero).append(' ').append(one).append('\n');
-		}
-		System.out.println(sb);
- 
-	}
- 
-	public static void fibonacci(int N) {
-		// 반드시 초기화 해야한다.
-		zero = 1;
-		one = 0;
-		zero_plus_one = 1;
- 
-		for (int i = 0; i < N; i++) {
-			zero = one;
-			one = zero_plus_one;
-			zero_plus_one = zero + one;
-		}
- 
-	}
- 
+    static int zero;
+    static int one;
+    static int zeroPlusOne;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        int T = readInt();
+
+        while (T-- > 0) {
+            int N = readInt();
+            fibonacci(N);
+            sb.append(zero).append(' ').append(one).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+
+    public static void fibonacci(int N) {
+        zero = 1;
+        one = 0;
+        zeroPlusOne = 1;
+
+        while (N-- > 0) {
+            zero = one;
+            one = zeroPlusOne;
+            zeroPlusOne = zero + one;
+        }
+    }
+
+    static int readInt() throws IOException {
+        int c, n = System.in.read() & 15;
+
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        if (c == 13) {
+            System.in.read();
+        }
+
+        return n;
+    }
 }
