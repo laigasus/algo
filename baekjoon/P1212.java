242c3ed6ad6deaa15d582c3b8299214116200a74
laigasus <okjaeook98@gmail.com>
2023-06-18 18:20:15 +0900

    20230618

--- baekjoon/P1212.java
+++ baekjoon/P1212.java
@@ -1,12 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class P1212 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger octal = new BigInteger(br.readLine(), 8);
-        System.out.println(octal.toString(2));
+        StringBuilder sb = new StringBuilder();
+        String[] val = { "000", "001", "010", "011", "100", "101", "110", "111" };
+
+        String octal = br.readLine();
+
+        for (int i = 0; i < octal.length(); i++) {
+            sb.append(val[octal.charAt(i) - '0']);
+        }
+
+        switch (octal.charAt(0)) {
+            case '0':
+            case '1':
+                sb.delete(0, 2);
+                break;
+            case '2':
+            case '3':
+                sb.deleteCharAt(0);
+                break;
+        }
+
+        System.out.println(sb);
     }
 }
--- baekjoon/P1373.java
+++ baekjoon/P1373.java
@@ -1,13 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class P1373 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger bin = new BigInteger(br.readLine(), 2);
+        StringBuilder sb = new StringBuilder();
 
-        System.out.println(bin.toString(8));
+        String bin = br.readLine();
+
+        if (bin.length() % 3 != 0) {
+            bin = "0".repeat(3 - bin.length() % 3) + bin;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < bin.length(); i++) {
+            if (bin.charAt(i) == '1') {
+                sum = sum + (4 >> (i % 3));
+            }
+            if ((i + 1) % 3 == 0) {
+                sb.append(sum);
+                sum = 0;
+            }
+        }
+
+        System.out.println(sb);
     }
 }
