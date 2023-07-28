b105880ebbfc8bb9232fe58ec65482cd9e3e0c79
laigasus <okjaeook98@gmail.com>
2023-07-28 16:01:12 +0900

    update P2580.java

--- baekjoon/P2580.java
+++ baekjoon/P2580.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringJoiner;
 import java.util.StringTokenizer;
 
-// 참조: https://st-lab.tistory.com/119
 public class P2580 {
     static final int BD_SIZE = 9;
+    static final int BD_SIZE_S = 3;
+    static final String SPACE = " ";
+
     static int[][] sudoku = new int[BD_SIZE][BD_SIZE];
 
     public static void main(String[] args) throws IOException {
@@ -23,20 +26,19 @@ public class P2580 {
 
     public static void calc(int row, int col) {
 
-        // 해당 행이 다 채워졌을 경우 다음 행의 첫 번째 열부터 시작
-        if (col == 9) {
+        if (col == BD_SIZE) {
             calc(row + 1, 0);
             return;
         }
 
-        // 행과 열이 모두 채워졌을 경우 출력 후 종료
-        if (row == 9) {
+        if (row == BD_SIZE) {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sb.append(sudoku[i][j]).append(' ');
+            for (int i = 0; i < BD_SIZE; i++) {
+                StringJoiner sj = new StringJoiner(SPACE);
+                for (int j = 0; j < BD_SIZE; j++) {
+                    sj.add(Integer.toString(sudoku[i][j]));
                 }
-                sb.append('\n');
+                sb.append(sj).append('\n');
             }
             System.out.print(sb);
 
@@ -46,7 +48,7 @@ public class P2580 {
 
         // 미 기입된 값 확인
         if (sudoku[row][col] == 0) {
-            for (int i = 1; i <= 9; i++) {
+            for (int i = 1; i <= BD_SIZE; i++) {
                 if (check(row, col, i)) {
                     sudoku[row][col] = i;
                     calc(row, col + 1);
@@ -57,28 +59,27 @@ public class P2580 {
         }
 
         calc(row, col + 1);
-
     }
 
     static boolean check(int row, int col, int v) {
         // 수평 검사
-        for (int idx = 0; idx < 9; idx++) {
+        for (int idx = 0; idx < BD_SIZE; idx++) {
             if (sudoku[row][idx] == v) {
                 return false;
             }
         }
 
         // 수직 검사
-        for (int idx = 0; idx < 9; idx++) {
+        for (int idx = 0; idx < BD_SIZE; idx++) {
             if (sudoku[idx][col] == v) {
                 return false;
             }
         }
 
         // 박스 검사
-        int pfRow = (row / 3) * 3, pfCol = (col / 3) * 3;
-        for (int i = pfRow; i < pfRow + 3; i++) {
-            for (int j = pfCol; j < pfCol + 3; j++) {
+        int pfRow = (row / BD_SIZE_S) * BD_SIZE_S, pfCol = (col / BD_SIZE_S) * BD_SIZE_S;
+        for (int i = pfRow; i < pfRow + BD_SIZE_S; i++) {
+            for (int j = pfCol; j < pfCol + BD_SIZE_S; j++) {
                 if (sudoku[i][j] == v) {
                     return false;
                 }
