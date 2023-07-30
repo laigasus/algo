4f69d9dfc07273e1a856a1f2969a3a06fce2577b
laigasus <okjaeook98@gmail.com>
2023-07-30 14:56:22 +0900

    readInt() 수정
    그동안 참고해서 이용해온 readInt 메서드가 잘못되었다고 판단
    직접 구현함

--- baekjoon/P11053.java
+++ baekjoon/P11053.java
@@ -25,17 +25,21 @@ public class P11053 {
         System.out.println(max);
     }
 
-    static int readInt() throws IOException {
-        int c, n = System.in.read() & 15;
-
-        while ((c = System.in.read()) > 32) {
-            n = (n << 3) + (n << 1) + (c & 15);
+    private static int readInt() throws IOException {
+        int rs = 0;
+        boolean isNegative = false;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
         }
-
-        if (c == 13) {
-            System.in.read();
+        if (c == '-') {
+            isNegative = true;
+            c = System.in.read();
         }
-
-        return n;
+        while (c >= '0' && c <= '9') {
+            rs = rs * 10 + c - '0';
+            c = System.in.read();
+        }
+        return isNegative ? -rs : rs;
     }
 }
