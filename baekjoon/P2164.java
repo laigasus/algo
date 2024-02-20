734358fe1b6af2c6b5a100a7ee0b7ae5fb919709
Ok JaeOok <okjaeook98@gmail.com>
2024-02-20 21:59:40 +0900

    20240220

--- baekjoon/P2164.java
+++ baekjoon/P2164.java
@@ -2,20 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class P2164 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        final int N = Integer.parseInt(br.readLine());
 
-        ArrayDeque<Integer> arr = new ArrayDeque<>(IntStream.rangeClosed(1, N).boxed().collect(Collectors.toList()));
-
-        while (arr.size() > 1) {
-            arr.removeFirst();
-            arr.add(arr.pollFirst());
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int num = N; num > 0; num--) {
+            deque.offerLast(num);
         }
-        System.out.println(arr.getFirst());
+
+        while (deque.size() > 1) {
+            deque.removeLast();
+            deque.offerFirst(deque.pollLast());
+        }
+
+        System.out.println(deque.peek());
     }
 }
