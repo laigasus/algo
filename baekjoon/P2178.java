e1727874f4fa8ebb79c7045ffb8789725bc8ff31
Ok JaeOok <okjaeook98@gmail.com>
2024-02-26 23:33:51 +0900

    20240226

--- baekjoon/P2178.java
+++ baekjoon/P2178.java
@@ -6,75 +6,79 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P2178 {
-    static Queue<Node> queue = new LinkedList<>();
-    static int[][] graph;
-    static int N, M;
-    // 이동방향 정의
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
-    final static int directionPoint = 4;
-
-    private static class Node {
-        int x;
-        int y;
-
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        final int N = Integer.parseInt(st.nextToken());
+        final int M = Integer.parseInt(st.nextToken());
 
-        graph = new int[N][M];
+        int[][] board = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                graph[i][j] = str.charAt(j) - '0';
+                board[i][j] = str.charAt(j) - '0';
             }
         }
 
-        System.out.println(bfs(0, 0));
+        System.out.println(calc(board));
     }
 
-    static int bfs(int x, int y) {
-        queue.offer(new Node(x, y));
+    private static class Point {
+        int x, y;
+
+        public Point(int y, int x) {
+            this.x = x;
+            this.y = y;
+        }
+
+        static final Point[] dir = {
+                new Point(0, -1),
+                new Point(-1, 0),
+                new Point(0, 1),
+                new Point(1, 0),
+        };
+    }
+
+    private static int calc(int[][] board) {
+        final int N = board.length;
+        final int M = board[0].length;
+
+        Queue<Point> queue = new LinkedList<>();
+
+        queue.offer(new Point(0, 0));
 
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            x = node.x;
-            y = node.y;
+            var now = queue.poll();
 
-            // 이동방향(동,서,남,북 +1)
-            for (int i = 0; i < directionPoint; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
+            for (var d : Point.dir) {
+                int nx = now.x + d.x;
+                int ny = now.y + d.y;
 
-                // 범위 밖인 경우
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                if (!isBoundary(N, M, nx, ny)) {
                     continue;
                 }
 
-                // 벽이 존재하는 경우
-                if (graph[nx][ny] == 0) {
+                if (board[ny][nx] == 0) {
                     continue;
                 }
 
-                // 한번도 지나지 않은 곳일 경우
-                if (graph[nx][ny] == 1) {
-                    // 누적 이동수를 그래프에 기록
-                    graph[nx][ny] = graph[x][y] + 1;
-                    queue.add(new Node(nx, ny));
+                if (board[ny][nx] == 1) {
+                    board[ny][nx] = board[now.y][now.x] + 1;
+                    var next = new Point(ny, nx);
+                    queue.offer(next);
                 }
             }
         }
 
-        // 종착지의 누적 이동수 출력
-        return graph[N - 1][M - 1];
+        return board[N - 1][M - 1];
     }
+
+    private static boolean isBoundary(final int N, final int M, int nx, int ny) {
+        return (nx < 0 || ny < 0 || nx >= M || ny >= N) ? false : true;
+    }
+
 }
