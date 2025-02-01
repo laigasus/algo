25b2dede1f95f64c3285a40e00d01681e4c16ae0
Ok JaeOok <okjaeook98@gmail.com>
2025-02-01 21:34:25 +0900

    20250201

--- baekjoon/P9655_2.java
+++ baekjoon/P9655_2.java
@@ -11,10 +11,19 @@ public class P9655_2 {
         int[] dp = new int[N + 1];
 
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 1;
-        for (int i = 4; i <= N; i++) {
-            dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+
+        if (N > 1) {
+            dp[2] = 2;
+        }
+
+        if (N > 2) {
+            dp[3] = 1;
+        }
+
+        if (N > 3) {
+            for (int i = 4; i <= N; i++) {
+                dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+            }
         }
 
         StringBuilder sb = new StringBuilder();
